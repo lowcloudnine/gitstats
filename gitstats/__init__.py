4409b55e9970cbfa7076f9ee2f2276c191e4b153
Jon Schiefelbein <lowcloudnine@hotmail.com>
2016-03-27 00:46:17 -0400

    added get_functions and git_dat... to __init__

--- gitstats/__init__.py
+++ gitstats/__init__.py
@@ -1,2 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+gitstats Package
+----------------
+
+The work horses of the gitstats program in a nice Python package.
+
+--------
+"""
+
 from . import conf
 from . import data_collector
+from . import git_data_collector
